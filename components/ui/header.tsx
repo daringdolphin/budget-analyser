c6b66a31bb162fc8508ca8483431ed09a6c8a095
daringdolphin <102495794+daringdolphin@users.noreply.github.com>
2025-03-03 02:31:57 +0800

    added sg flag

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,17 +1,24 @@
+import Image from "next/image";
 export default function Header() {
   return (
     <header className="text-center pt-8 pb-4">
-      {/* <div className="flex items-center justify-center mb-4">
-        <Flag className="h-8 w-8 text-red-600 mr-2" />
-      </div> */}
-
       <h1 className="text-4xl md:text-5xl font-bold tracking-tight bg-gradient-to-r from-slate-800 to-slate-600 bg-clip-text text-transparent leading-[1.15] pb-1">
         Discover a FY2025 Budget for you
       </h1>
       <p className="flex items-center justify-center text-lg text-slate-600 max-w-2xl mx-auto mt-6 gap-2">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg" alt="Flag of Singapore" className="w-6 h-6" />
+        <Image 
+          src="https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg" 
+          alt="Flag of Singapore" 
+          width={24}
+          height={24}
+        />
         Onward Together For A Better Tomorrow
-        <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg" alt="Flag of Singapore" className="w-6 h-6" />
+        <Image 
+          src="https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg" 
+          alt="Flag of Singapore"
+          width={24} 
+          height={24}
+        />
       </p>
     </header>
   )
